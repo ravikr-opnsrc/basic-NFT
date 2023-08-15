1ebceea328eeb1bfb39572936074f9d7f92bafb5
ravikr-opnsrc <ravikr.opnsrc@gmail.com>
2023-08-15 23:01:50 +0530

    basic NFT contract on Ethereum

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const nftContract = await hre.ethers.deployContract("NFTee");
+
+  await nftContract.waitForDeployment();
+
+  console.log("NFT Contract Address:", nftContract.target);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
